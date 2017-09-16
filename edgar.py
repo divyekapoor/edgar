f20537bf19e2f280cd7322938c11833d1f8ab537
Divye Kapoor <divyekapoor@gmail.com>
2017-09-16 12:32:32 -0700

    Correctly identify summary of significant accounting policy sheets.

--- edgar.py
+++ edgar.py
@@ -3,11 +3,14 @@ import csv
 import logging
 import os
 import sys
+from functools import total_ordering
 from optparse import OptionParser
 
 import openpyxl
 import requests
+import requests_cache
 from typing import List
+from openpyxl.worksheet import Worksheet
 
 USAGE = """
         Edgar Summary of Accounting Policies Extractor
@@ -51,6 +54,77 @@ class OutputRow:
         self.text = text
 
 
+@total_ordering
+class XLSWorksheet:
+    def __init__(self, worksheet_index: int,
+                 worksheet: openpyxl.worksheet.Worksheet):
+        self.worksheet = worksheet
+        self.rows = list(worksheet.values)
+        self.num_rows = len(self.rows)
+        self.worksheet_index = worksheet_index
+        self.rows_without_tables = [
+            row for row in self.rows if self._allColumnsAfterBAreNone(row)]
+        self.num_non_table_rows = len(self.rows_without_tables)
+
+    def is_summary_of_accounting_policies_sheet(self) -> bool:
+        """
+        Heuristics for detecting the summary of accounting policy sheet.
+        We might be wrong (sometimes).
+        
+        1. Sheet must have more than 50 rows. 50 is tweakable.
+        2. Sheet must have more than 50 non table rows. 50 is tweakable.
+        3. First 10 rows must contain "Summary of Significant Accounting 
+        Policies". 10 is tweakable.
+        4. First 10 rows must contain "Significant Accounting Policies". 10 
+        is tweakable.
+        
+        In all the examples, I've seen that the term is present in the first 
+        4 or 5 rows. 
+        """
+        if self.num_rows < 50:
+            return False
+
+        if self.num_non_table_rows < 50:
+            return False
+
+        for row in self.rows[0:min(10, len(self.rows))]:
+            for values in row:
+                if values is None:
+                    continue
+                if not isinstance(values, str):
+                    continue
+                row_value = values  # type: str
+                row_value = row_value.lower()
+                if 'summary of significant accounting policies' in row_value:
+                    return True
+                if 'significant accounting policies' in row_value:
+                    return True
+        return False
+
+    def _allColumnsAfterBAreNone(self, row):
+        slice = row[2:]
+        for entry in slice:
+            if entry is not None:
+                return False
+        return True
+
+    def __str__(self):
+        return '(rows: {}, non_table_rows: {}, worksheet_index: {}, {})'.format(
+            self.num_rows, self.num_non_table_rows,
+            self.worksheet_index, self.worksheet.title)
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __lt__(self, other):
+        return self.num_rows < other.num_rows or (
+            self.num_rows == other.num_rows
+            and self.worksheet_index < other.worksheet_index)
+
+    def __eq__(self, other):
+        return self.worksheet == other.worksheet
+
+
 class InputRow:
     def __init__(self, id_value: str, edgar_url: str):
         self.id = id_value
@@ -74,11 +148,14 @@ class InputRow:
         results = []
         workbook = openpyxl.load_workbook(self.xls_file)
         top_sheets = sorted(
-            [(len(list(worksheet.values)), index, worksheet.title)
+            [XLSWorksheet(index, worksheet)
              for index, worksheet in enumerate(workbook.worksheets)],
             reverse=True)
         logger.info('Top sheets: %s', top_sheets)
-
+        accounting_policy_sheets = [
+            worksheet for worksheet in top_sheets
+            if worksheet.is_summary_of_accounting_policies_sheet()]
+        logger.info('Accounting Policy sheets: %s', accounting_policy_sheets)
 
         return results
 
@@ -127,6 +204,8 @@ def main():
         inputs = [InputRow(record['ID'], record['URL']) for record in reader]
         logger.info('Inputs: %s', inputs)
         for input in inputs:
+            requests_cache.install_cache(os.path.abspath(os.path.join(
+                options.workdir, 'requests_cache')))
             input.fetch_file(options.workdir)
             input.get_accounting_policies()
 
